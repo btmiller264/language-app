915b015dad2c0328683d5a7590b1e2b8aa59b008
Braden <btmiller264@gmail.com>
2023-01-21 19:40:51 -0700

    Added initial redux store

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export const App: FC<any> = () => {
     return (
-        <div>This is the app!</div>
+        <Provider store={store}>
+            <div>This is the app!</div>
+        </Provider>
+
     )
 }
